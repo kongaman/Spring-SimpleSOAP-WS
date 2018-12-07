8600e78642acd60e7cbe1d0b819753c760fb1921
kongaman <christian.kranz@gmail.com>
2018-12-07 15:27:14 +0100

    unsaved class

--- src/main/java/com/ckranz/ws/soap/HelloWS.java
+++ src/main/java/com/ckranz/ws/soap/HelloWS.java
@@ -1,7 +1,12 @@
 package com.ckranz.ws.soap;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
 public class HelloWS {
 	
+	@WebMethod
 	public String hello() {
 		return "hello";
 	}
